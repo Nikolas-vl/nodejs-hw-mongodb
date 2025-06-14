f0029e1a328d5b18a7f9c49f55cd6dc17577565f
Nikolas-vl <nikvv02@gmail.com>
2025-06-14 21:58:56 +0200

    http-errors

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,14 +10,12 @@ export const getContactsController = async (req, res) => {
   });
 };
 
-export const getContactByIdController = async (req, res) => {
+export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    res.status(404).json({
-      message: 'Contact not found',
-    });
+    next(new Error('Contact not found'));
     return;
   }
 
