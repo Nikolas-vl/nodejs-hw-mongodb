d0eeb8924aa91bf562684310ebfff5ec7863414f
Nikolas-vl <nikvv02@gmail.com>
2025-06-15 19:54:37 +0200

    error middlewares

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
 import router from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -21,18 +23,9 @@ export const setupServer = () => {
 
   app.use(router);
 
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use(notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
